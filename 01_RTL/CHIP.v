a5ed7d65185a09952b3bbd4876df2d1256a5c463
Sean Fu <SeanFu920506@gmail.com>
2023-12-01 14:06:38 +0800

    add basic

--- 01_RTL/CHIP.v
+++ 01_RTL/CHIP.v
@@ -51,16 +51,17 @@ module CHIP #(                                                                  
 // ------------------------------------------------------------------------------------------------------------------------------------------------------
 
     // TODO: Reg_file wire connection
+    // FIXME: fixed the input of regfile
     Reg_file reg0(               
         .i_clk  (i_clk),             
         .i_rst_n(i_rst_n),         
-        .wen    (),          
-        .rs1    (),                
-        .rs2    (),                
-        .rd     (),                 
-        .wdata  (),             
-        .rdata1 (),           
-        .rdata2 ()
+        .wen    (wb_regwrite),          
+        .rs1    (id_rs1_w),                
+        .rs2    (id_rs2_w),                
+        .rd     (id_rd_w),                 
+        .wdata  (wb_rdata),             
+        .rdata1 (rs1_output),           
+        .rdata2 (rs2_output)
     );
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -68,12 +69,331 @@ module CHIP #(                                                                  
 // ------------------------------------------------------------------------------------------------------------------------------------------------------
     
 
-//TODO: IF
-//TODO: ID
-//TODO: EX
-//TODO: MEM
-//TODO: WB
 
+//TODO: ID
+    reg [4:0]  id_rs1_r, id_rs1_w, id_rs2_r, id_rs2_w, id_rd_r, id_rd_w;
+    // reg [6:0]  id_opcode_r, id_opcode_w;
+    reg [2:0]  id_func3_r, id_func3_w;
+    reg [6:0]  id_func7_r, id_func7_w;
+    reg [63:0] id_imm_r, id_imm_w;
+    reg id_branch_r, id_branch_w;
+    reg id_jump_r, id_jump_w;
+    reg id_memread_r, id_memread_w;
+    reg id_memtoreg_r, id_memtoreg_w;
+    reg [1:0] id_alu_op_r, id_alu_op_w;
+    reg id_memwrite_r, id_memwrite_r;
+    reg id_alu_src_r, id_alu_src_w;
+    reg id_regwrite_r, id_regwrite_w;
+    always @(*) begin
+        if(i_DMEM_stall == 0) begin
+            id_rs1_w   = i_IMEM_data[19:15];
+            id_rs2_w   = i_IMEM_data[24:20];
+            id_rd_w    = i_IMEM_data[11:7];
+            id_func3_w = i_IMEM_data[14:12]; 
+            id_func7_w = i_IMEM_data[31:25];
+            if(i_IMEM_data[6:0] == 7'b1100011)begin //branch (SB type) //FINISH
+                id_branch_w   = 1;
+                id_memread_w  = 0;
+                id_memtoreg_w = 0;
+                id_alu_op_w   = 2'b01;
+                id_memwrite_w = 0;
+                id_alu_src_w  = 0; // 0 : from another register, 1 : from immediate 
+                id_regwrite_w = 0;
+                id_imm_w      = 0;
+                id_jump_w     = 0;
+            end
+            else if(i_IMEM_data[6:0] == 7'b0010111)begin //auipc (U type) //FINISH
+                id_branch_w   = 0;
+                id_memread_w  = 0;
+                id_memtoreg_w = 0;
+                id_alu_op_w   = 2'b11;
+                id_memwrite_w = 0;
+                id_alu_src_w  = 1; // 0 : from another register, 1 : from immediate   
+                id_regwrite_w = 1;
+                id_imm_w[63:32] = 0;
+                id_imm_w[31:12] = i_IMEM_data[31:12];
+                id_imm_w[11:0]  = 0;      
+                id_jump_w       = 0;
+            end
+            else if(i_IMEM_data[6:0] == 7'b1101111)begin //jal (UJ type) //FINISH
+                id_branch_w   = 1;
+                id_memread_w  = 0;
+                id_memtoreg_w = 0;
+                id_alu_op_w   = 2'b01;
+                id_memwrite_w = 0;
+                id_alu_src_w  = 1; // 0 : from another register, 1 : from immediate 
+                id_regwrite_w = 1;
+                id_imm_w[63:21] = 0;
+                id_imm_w[20]    = i_IMEM_data[31];
+                id_imm_w[19:12] = i_IMEM_data[19:12];
+                id_imm_w[11]    = i_IMEM_data[20];
+                id_imm_w[10:1]  = i_IMEM_data[30:21];
+                id_imm_w[0]     = 0;
+                id_jump_w       = 1;
+            end
+            else if(i_IMEM_data[6:0] == 7'b1100111)begin //jalr (I type) //FINISH
+                id_branch_w     = 1;
+                id_memread_w    = 0;
+                id_memtoreg_w   = 0;
+                id_alu_op_w     = 2'b01;
+                id_memwrite_w   = 0;
+                id_alu_src_w    = 1;
+                id_regwrite_w   = 1;
+                id_imm_w        = 0;
+                id_jump_w       = 1;
+            end
+            else if(i_IMEM_data[6:0] == 7'b0000011)begin //lw  (I type) //FINISH
+                id_branch_w   = 0;
+                id_memread_w  = 1;
+                id_memtoreg_w = 1;
+                id_alu_op_w   = 2'b00;
+                id_memwrite_w = 0;
+                id_alu_src_w  = 1; // 0 : from another register, 1 : from immediate 
+                id_regwrite_w = 1;
+                id_imm_w[63:12] = 0;
+                id_imm_w[11:0]  = i_IMEM_data[31:20];
+                id_jump_w       = 0;
+            end
+            else if(i_IMEM_data[6:0] == 7'b0100011)begin //sw  (S type) //FINISH
+                id_branch_w   = 0;
+                id_memread_w  = 0;
+                id_memtoreg_w = 0;
+                id_alu_op_w   = 2'b00;
+                id_memwrite_w = 1;
+                id_alu_src_w  = 1; // 0 : from another register, 1 : from immediate
+                id_regwrite_w = 0;
+                id_imm_w[63:12] = 0;
+                id_imm_w[11:5]  = i_IMEM_data[31:25];
+                id_imm_w[4:0]   = i_IMEM_data[11:7]; 
+                id_jump_w       = 0;           
+            end
+            else if(i_IMEM_data[6:0] == 7'b0010011)begin //addi (I type) //FINISH
+                id_branch_w   = 0;
+                id_memread_w  = 0;
+                id_memtoreg_w = 0;
+                id_alu_op_w   = 2'b10;
+                id_memwrite_w = 0;
+                id_alu_src_w  = 1; // 0 : from another register, 1 : from immediate 
+                id_regwrite_w = 1;
+                id_imm_w[63:12] = 0;
+                id_imm_w[11:0]  = i_IMEM_data[31:20];
+                id_jump_w       = 0;
+            end
+            else if(i_IMEM_data[6:0] == 7'b0110011)begin //add  (R type) //FINISH
+                id_branch_w   = 0;
+                id_memread_w  = 0;
+                id_memtoreg_w = 0;
+                id_alu_op_w   = 2'b10;
+                id_memwrite_w = 0;
+                id_alu_src_w  = 0; // 0 : from another register, 1 : from immediate 
+                id_regwrite_w = 1;
+                id_imm_w      = 0;
+                id_jump_w     = 0;
+            end
+            else if(i_IMEM_data[6:0] == 7'b1110011)begin //ecall (I type)  //FINISH
+                id_branch_w   = 0;
+                id_memread_w  = 0;
+                id_memtoreg_w = 0;
+                id_alu_op_w   = 2'b00;
+                id_memwrite_w = 0;
+                id_alu_src_w  = 0; // 0 : from another register, 1 : from immediate 
+                id_regwrite_w = 0;
+                id_imm_w      = 0;
+                id_jump_w     = 0;
+            end
+            else begin  //FINISH
+                id_branch_w   = 0;
+                id_memread_w  = 0;
+                id_memtoreg_w = 0;
+                id_alu_op_w   = 2'b00;
+                id_memwrite_w = 0;
+                id_alu_src_w  = 0; // 0 : from another register, 1 : from immediate 
+                id_regwrite_w = 0;
+                id_imm_w      = 0;
+                id_jump_w     = 0;
+            end
+        end
+        else begin
+            id_rs1_w      = i_IMEM_data[19:15];
+            id_rs2_w      = i_IMEM_data[24:20];
+            id_rd_w       = i_IMEM_data[11:7];
+            id_func3_w    = i_IMEM_data[14:12]; 
+            id_func7_w    = i_IMEM_data[31:25];
+            id_branch_w   = id_branch_r;
+            id_memread_w  = id_memread_r;
+            id_alu_op_w   = id_alu_op_r;
+            id_memwrite_w = id_memwrite_r;
+            id_alu_src_w  = id_alu_src_r;
+            id_regwrite_w = id_regwrite_r;
+            id_imm_w      = id_imm_r;  
+            id_jump_w     = id_jump_r;
+        end
+    end
+
+//TODO: EX
+    reg [31:0] rs1_output, rs2_output;
+    // reg [31:0] ex_rs1_data_w, ex_rs1_data_r;
+    // reg [31:0] ex_rs2_data_w, ex_rs2_data_r;
+    reg [31:0] ex_pc_cal_w, ex_pc_cal_r;
+    reg [31:0] ex_result_w, ex_result_r;
+    reg ex_memread_r, ex_memread_w;
+    reg ex_memtoreg_r, ex_memtoreg_w;
+    reg ex_memwrite_r, ex_memwrite_w;
+    reg ex_regwrite_r, ex_regwrite_w;
+    reg ex_rd_r, ex_rd_w;
+    reg ex_branch_r, ex_branch_w;
+    always @(*) begin
+        if(i_DMEM_stall == 0) begin
+            ex_pc_cal_w   = pc + id_imm_r;
+            ex_memread_w  = id_memread_r;
+            ex_memtoreg_w = id_memtoreg_r;
+            ex_memwrite_w = id_memwrite_r;
+            ex_regwrite_w = id_regwrite_r; 
+            ex_rd_w       = id_rd_r;
+            ex_branch_w   = id_branch_r;
+            if(id_alu_src == 0) begin // alu source is reg
+                if(id_alu_op_r == 2'b00) begin
+                    ex_result_w = rs1_output + rs2_output;
+                end
+                else if(id_alu_op_r == 2'b01) begin //branch
+                    if(id_func3_r == 3'b000) begin
+                        
+                    end
+                    else if(id_func3_r == 3'b001) begin
+                        
+                    end
+                    else if(id_func3_r == 3'b100) begin
+                    end
+                    else if(id_func3_r == 3'b101) begin
+                        
+                    end
+                    else begin
+                        ex_result_w = rs1_output - rs2_output;
+                    end
+                end
+                else if(id_alu_op_r == 2'b10) begin
+                    if(id_func3_r == 3'b000 && id_func7_r == 7'b0000000) begin //add
+                        ex_result_w = rs1_output + rs2_output;
+                    end
+                    else if(id_func3_r == 3'b000 && id_func7_r == 7'b0100000) begin //sub
+                        ex_result_w = $signed(rs1_output) - $signed(rs2_output);
+                    end
+                    else if(id_func3_r = 3'b100 && id_func7_r == 7'b0000000) begin //xor
+                        ex_result_w = rs1_output ^ rs2_output;
+                    end
+                    else if(id_func3_r = 3'b111 && id_func7_r == 7'b0000000) begin //and
+                        ex_result_w = rs1_output & rs2_output;
+                    end
+                    else begin //mul
+                        ex_result_w = rs1_output * rs2_output; //FIXME: change this into alu in HW2
+                    end
+                end
+                else if(id_alu_op_r == 2'b11) begin //Nothing
+                    ex_result_w = 0;
+                end
+            end
+            else begin           // alu source is imm
+                if(id_alu_op_r == 2'b00) begin // lw, sw
+                    ex_result_w = rs1_output + id_imm_r;
+                end
+                else if(id_alu_op_r == 2'b01) begin //jal, jalr
+                    ex_result_w = PC + 4;
+                end
+                else if(id_alu_op_r == 2'b10) begin //I type
+                    if(id_func3_r == 3'b000) begin // addi
+                        ex_result_w = rs1_output + id_imm_r;
+                    end
+                    else if(id_func3_r == 3'b010) begin //slti
+                        if($signed(rs1_output) < $signed(id_imm_r[11:0])) begin
+                            ex_result_w = 1;
+                        end
+                        else begin
+                            ex_result_w = 0;
+                        end
+                    end
+                    else if(id_func3_r == 3'b001) begin //slli
+                        ex_result_w = rs1_output << id_imm_r;
+                    end
+                    else if(id_func3_r == 3'b101) begin //srai
+                        ex_result_w = $signed(rs1_output) >>> id_imm_r;
+                    end
+                    else begin //Nothing
+                        ex_result_w = 0;
+                    end
+                end
+                else if(id_alu_op_r == 2'b11) begin //auipc
+                    ex_result_w = id_imm_r[31:0];
+                end
+            end
+        end
+        else begin
+            ex_pc_cal_w   = ex_pc_cal_r;
+            ex_memread_w  = ex_memread_r;
+            ex_memtoreg_w = ex_memtoreg_r;
+            ex_memwrite_w = ex_memwrite_r;
+            ex_regwrite_w = ex_regwrite_r; 
+            ex_rd_w       = ex_rd_r;
+            ex_result_w   = ex_result_r;
+            ex_branch_w   = ex_branch_r;
+        end
+    end
+//TODO: MEM
+    reg [31:0] mem_out_w, mem_out_r;
+    reg mem_rd_w, mem_rd_r;
+    reg mem_result_w, mem_result_r;
+    reg mem_memtoreg_w, mem_memtoreg_r;
+    reg mem_regwrite_w, mem_regwrite_r;
+    always @(*) begin
+        if(i_DMEM_stall == 0) begin
+            mem_rd_w       = ex_rd_r;
+            mem_result_w   = ex_result_r;
+            mem_memtoreg_w = ex_memtoreg_r;
+            mem_regwrite_w = ex_regwrite_r;
+            if(ex_memread_r || ex_memwrite_r) begin
+                if(ex_memread_r) begin
+                    mem_out_w = i_DMEM_rdata;
+                end
+                else begin
+                    mem_out_w = 0;
+                end
+            end
+            else begin
+                mem_out_w = 0;
+            end
+        end
+        else begin
+            mem_rd_w       = mem_rd_r;
+            mem_result_w   = mem_result_r;
+            mem_memtoreg_w = mem_memtoreg_r;
+            mem_regwrite_w = mem_regwrite_r;
+            mem_out_w      = mem_out_r;
+        end
+    end
+
+
+//TODO: WB
+    reg wb_regwrite;
+    reg [31:0] wb_rdata;
+    always @(*) begin
+        if(mem_regwrite_r) begin
+            wb_regwrite = 1;
+            wb_rdata = ex_result_r;
+        end
+        else begin
+            wb_regwrite = 0;
+            wb_rdata = 0;
+        end
+    end
+
+//TODO: Branch
+    always @(*) begin
+        if(ex_branch_r && mem_result_r == 0) begin
+            next_PC = ex_pc_cal_r;
+        end
+        else begin
+            next_PC = PC + 4;
+        end
+    end
 
     // Todo: any combinational/sequential circuit
 
@@ -82,7 +402,33 @@ module CHIP #(                                                                  
             PC <= 32'h00010000; // Do not modify this value!!!
         end
         else begin
-            PC <= next_PC;
+            PC             <= next_PC;
+            id_rs1_r       <= id_rs1_w;
+            id_rs2_r       <= id_rs2_w;
+            id_rd_r        <= id_rd_w;
+            id_branch_r    <= id_branch_w;
+            id_memread_r   <= id_memread_w;
+            id_memtoreg_r  <= id_memtoreg_w;
+            id_alu_op_r    <= id_alu_op_w;
+            id_memwrite_r  <= id_memwrite_w;
+            id_alu_src_r   <= id_alu_src_w;
+            id_regwrite_r  <= id_regwrite_w;
+            id_func3_r     <= id_func3_w;
+            id_func7_r     <= id_func7_w;
+            id_jump_r      <= id_jump_w;
+            ex_pc_cal_r    <= ex_pc_cal_w;
+            ex_memread_r   <= ex_memread_w;
+            ex_memtoreg_r  <= ex_memtoreg_w;
+            ex_memwrite_r  <= ex_memwrite_w;
+            ex_regwrite_r  <= ex_regwrite_w; 
+            ex_rd_r        <= ex_rd_w;
+            ex_result_r    <= ex_result_w;
+            ex_branch_r    <= ex_branch_w;
+            mem_rd_r       <= mem_rd_w;
+            mem_result_r   <= mem_result_w;
+            mem_memtoreg_r <= mem_memtoreg_w;
+            mem_regwrite_r <= mem_regwrite_w;
+            mem_out_r      <= mem_out_w;
         end
     end
 endmodule
